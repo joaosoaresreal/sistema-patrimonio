2f5c3c5a7d7eaaf1fc245475f5fdf105462c3f70
--global <--global>
2023-10-11 19:45:58 -0400

    Excluindo Usuarios

--- sistema-patrimonio-front/src/app/paginas/usuarios/listagem-usuarios/listagem-usuarios.page.ts
+++ sistema-patrimonio-front/src/app/paginas/usuarios/listagem-usuarios/listagem-usuarios.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
 import { UsuarioDTO } from 'src/app/models/UsuarioDTO';
 import { UsuarioService } from 'src/app/services/domain/Usuario.service';
 
@@ -11,7 +12,10 @@ export class ListagemUsuariosPage implements OnInit {
 
   usuarios!: UsuarioDTO[]
 
-  constructor(public usuarioService: UsuarioService) { }
+  public usuarioId: any
+  public disabled = false
+
+  constructor(public nav: NavController, public usuarioService: UsuarioService, private alertController: AlertController) { }
 
   /********************************************************\
                     LISTAGEM DOS USUARIOS 
@@ -22,6 +26,61 @@ export class ListagemUsuariosPage implements OnInit {
     }, error => {
       console.log(error)
     })
+  }
+
+  /********************************************************\
+                  SELEÇÃO DO CHECKBOX 
+  \********************************************************/
+  usuarioSelecionado(event: any) {
+    if(event.detail.checked) {
+      console.log(`Clicou no checkbox`, event);
+      this.usuarioId = event.detail.value
+
+      if(this.usuarioId !== event.detail.value){
+        this.disabled = false
+      } 
+      if(this.usuarioId === event.detail.value) {
+        this.disabled = true
+      }
+
+
+      console.log(this.usuarioId)
+
+    } else if (!event.detail.checked) {
+      console.log(`desclicou`, event);
+    }
+  }
+
+  /********************************************************\
+                    EXCLUIR USUARIO 
+  \********************************************************/
+  async excluirSelecionado() {
+    const alert = await this.alertController.create({
+      header: 'Deseja realmente excluir o usuário selecionado?',
+      buttons: [
+        {
+          text: 'SIM',
+          role: 'sim',
+          handler: () => {
+            this.usuarioService.delete(this.usuarioId).subscribe({ next: (response)=> window.location.reload(),
+              error: (error)=> console.log(error)
+            })
+          },
+        },
+        {
+          text: 'NÃO',
+          role: 'nao',
+          handler: () => {
+            this.nav.navigateForward('listagem-usuarios')
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    //this.roleMessage = `Dismissed with role: ${role}`;
   }
 
   ngOnInit() {
--- sistema-patrimonio-front/src/app/services/domain/Usuario.service.ts
+++ sistema-patrimonio-front/src/app/services/domain/Usuario.service.ts
@@ -19,4 +19,21 @@ export class UsuarioService{
             observe: 'response', responseType: 'text'
         })
     }
+
+    // EDITAR USUÁRIO
+    update(usuario: UsuarioDTO){
+        return this.http.put(`${API_CONFIG.baseUrl}/api_usuarios/${usuario.id}`, usuario, {
+            observe: 'response', responseType: 'text'
+        })
+    }
+
+    // BUSCAR POR ID
+    findById(id: number):Observable<UsuarioDTO>{
+        return this.http.get<UsuarioDTO>(`${API_CONFIG.baseUrl}/api_usuarios/${id}`)
+    }
+
+    // EXLUIR USUÁRIO
+    delete(id: number){
+      return this.http.delete(`${API_CONFIG.baseUrl}/api_usuarios/${id}`)
+    }
 }
