c1b3dc25d8d1f9736d1bfd45db41fb36e3f2a874
joaosoaresreal <joao.soares8@estudante.ifms.edu.br>
2023-10-12 19:37:59 -0400

    Cadastrando Departamento

--- sistema-patrimonio-front/src/app/paginas/departamento/cadastrar-departamento/cadastrar-departamento.module.ts
+++ sistema-patrimonio-front/src/app/paginas/departamento/cadastrar-departamento/cadastrar-departamento.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { CadastrarDepartamentoPageRoutingModule } from './cadastrar-departamento-routing.module';
 
 import { CadastrarDepartamentoPage } from './cadastrar-departamento.page';
+import { DepartamentoService } from 'src/app/services/domain/Departamento.service';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    CadastrarDepartamentoPageRoutingModule
+    CadastrarDepartamentoPageRoutingModule,
+    ReactiveFormsModule
   ],
+  providers: [DepartamentoService],
   declarations: [CadastrarDepartamentoPage]
 })
 export class CadastrarDepartamentoPageModule {}
--- sistema-patrimonio-front/src/app/paginas/departamento/cadastrar-departamento/cadastrar-departamento.page.ts
+++ sistema-patrimonio-front/src/app/paginas/departamento/cadastrar-departamento/cadastrar-departamento.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertController, NavController } from '@ionic/angular';
+import { DepartamentoService } from 'src/app/services/domain/Departamento.service';
 
 @Component({
   selector: 'app-cadastrar-departamento',
@@ -7,9 +10,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarDepartamentoPage implements OnInit {
 
-  constructor() { }
+  cadastrarDepartamentoForm!: FormGroup
+
+  constructor(public nav: NavController, private alertController: AlertController, private formBuilder: FormBuilder, 
+    private departamentoService: DepartamentoService) { }
+
+  /********************************************************\
+                      SALVAR EDIÇÃO
+  \********************************************************/
+  submit() {
+    if (this.cadastrarDepartamentoForm.invalid || this.cadastrarDepartamentoForm.pending) {
+      return // CANCELA A SUBMISSAO E RETORNA OS ERROS PARA O USUÁRIO
+    }
+
+    let departamento = {
+      nome: this.cadastrarDepartamentoForm.value.nome,
+      telefone: this.cadastrarDepartamentoForm.value.telefone,
+      email: this.cadastrarDepartamentoForm.value.email,
+      endereco: this.cadastrarDepartamentoForm.value.endereco
+    }
+
+    this.departamentoService.insert(departamento).subscribe({
+      next: (response) =>
+        this.alerta('Usuário cadastrado com sucesso', 'OK', () => { this.nav.navigateForward('listagem-departamentos') }),
+      error: (error) => console.log(error)
+    })
+  }
 
   ngOnInit() {
+    this.cadastrarDepartamentoForm = this.formBuilder.group({
+      nome: ['', Validators.required],
+      telefone: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      endereco: ['', Validators.required],
+      empresa: {
+        id: 1
+      }
+    })
+  }
+
+  /********************************************************\
+                    MENSAGEM PADRÃO 
+  \********************************************************/
+  async alerta(header: string, text: string, handler: any) {
+    const alert = await this.alertController.create({
+      header,
+      buttons: [
+        {
+          text,
+          handler,
+        }
+      ],
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
   }
 
 }
