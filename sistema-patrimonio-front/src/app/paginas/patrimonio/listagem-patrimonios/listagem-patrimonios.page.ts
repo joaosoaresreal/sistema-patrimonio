d41f58bcae785042a7033784b8ad36b82d5ae5ff
--global <--global>
2023-10-11 20:01:52 -0400

    Teste commit

--- sistema-patrimonio-front/src/app/paginas/patrimonio/listagem-patrimonios/listagem-patrimonios.page.ts
+++ sistema-patrimonio-front/src/app/paginas/patrimonio/listagem-patrimonios/listagem-patrimonios.page.ts
@@ -28,7 +28,6 @@ export class ListagemPatrimoniosPage implements OnInit {
       console.log(error)
     })
   }
-  
 
   /********************************************************\
                   SELEÇÃO DO CHECKBOX 
