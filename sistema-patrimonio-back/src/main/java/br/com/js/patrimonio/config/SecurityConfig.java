b4a28dcdfeac36b2755624cbbb59fc134d9f81a7
João Soares <joao.soares8@estudante.ifms.edu.br>
2023-10-19 21:53:24 -0400

    Desenvolvendo o Spring Security

---
+++ sistema-patrimonio-back/src/main/java/br/com/js/patrimonio/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package br.com.js.patrimonio.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf(Csrf -> Csrf.disable());
+		http.authorizeHttpRequests(auth -> auth.requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**"))
+				.permitAll().anyRequest().permitAll());
+		http.headers(headers -> headers.frameOptions().disable());
+		return http.build();
+	}
+}
