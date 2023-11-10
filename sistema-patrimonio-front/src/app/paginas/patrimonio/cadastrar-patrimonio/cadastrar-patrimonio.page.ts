d32fe9e7d108822495a8204d48ebdabab8829739
João Soares <joao.soares8@estudante.ifms.edu.br>
2023-11-09 21:00:29 -0400

    Ajutes no cadastro de patrimonio

--- sistema-patrimonio-front/src/app/paginas/patrimonio/cadastrar-patrimonio/cadastrar-patrimonio.page.ts
+++ sistema-patrimonio-front/src/app/paginas/patrimonio/cadastrar-patrimonio/cadastrar-patrimonio.page.ts
@@ -4,6 +4,7 @@ import { AlertController, NavController } from '@ionic/angular';
 import { DepartamentoNomeDTO } from 'src/app/models/DepartamentoNomeDTO';
 import { DepartamentoService } from 'src/app/services/domain/Departamento.service';
 import { PatrimonioService } from 'src/app/services/domain/Patrimonio.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-cadastrar-patrimonio',
@@ -14,6 +15,8 @@ export class CadastrarPatrimonioPage implements OnInit {
 
   patrimonioForm!: FormGroup
   departamentos!: DepartamentoNomeDTO[]
+
+  plaqueta: any
 
   constructor(private formBuilder: FormBuilder, private patrimonioService: PatrimonioService,
     private departamentoService: DepartamentoService, private alertController: AlertController, public nav: NavController) { }
@@ -27,7 +30,7 @@ export class CadastrarPatrimonioPage implements OnInit {
     }
 
     let patrimonio = {
-      'plaqueta': this.patrimonioForm.value.plaqueta,
+      'plaqueta': this.plaqueta, // Envia a plaqueta em UpperCase para o banco
       'descricao': this.patrimonioForm.value.descricao,
       'estado': this.patrimonioForm.value.estado,
       'localizacao': this.patrimonioForm.value.localizacao,
@@ -37,6 +40,8 @@ export class CadastrarPatrimonioPage implements OnInit {
         'id': this.patrimonioForm.value.departamento,
       }
     }
+
+    console.log(patrimonio)
 
     this.patrimonioService.insert(patrimonio).subscribe({next: (response)=>
        this.alerta(),
@@ -54,6 +59,48 @@ export class CadastrarPatrimonioPage implements OnInit {
   })
   }
 
+  /********************************************************\
+                VERIFICAR SE PATRIMONIO JA 
+                ESTA CADASTRADO NO SISTEMA
+  \********************************************************/
+  verificaPlaqueta(){
+    let plaquetaInserida = this.patrimonioForm.value.plaqueta.toUpperCase()
+    this.patrimonioForm.get('plaqueta')?.setValue(plaquetaInserida)
+    this.plaqueta = plaquetaInserida
+
+    this.patrimonioService.findByPlaqueta(plaquetaInserida).subscribe((response) => {
+      if (response.id >= 1) {
+        this.alertaPlaqueta(response.id) // Se o CPF já existe, exiba o alerta e envia o 'id' para poder fazer o método de edição
+      }
+    }
+    )
+  }
+
+  alertaPlaqueta(id: number){
+    Swal.fire({
+      heightAuto: false, // Remove o 'heigth' que estava definido nativamente, pois ele quebra o estilo da pagina
+      allowOutsideClick: false, // Ao clicar fora do alerta ele não vai fechar
+      title: 'ATENÇÃO',
+      text: 'A plaqueta inserida já está cadastrada no sistema, deseja editar o cadastro?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'SIM',
+      cancelButtonText: 'NÃO',
+      // Customizção
+      confirmButtonColor: 'var(--ion-color-success-tint)',
+      cancelButtonColor: 'var(--ion-color-danger-tint)',
+      backdrop: `linear-gradient(#a24b7599 100%, transparent 555%)`
+    }).then((result) => {
+      if (result.isConfirmed) { // Se o resultado for 'SIM', faça isso
+        this.nav.navigateForward(`editar-patrimonio/${id}`)
+      } else if (
+        result.dismiss === Swal.DismissReason.cancel // Se o resultado for 'NÃO', faça isso
+      ) {
+        this.nav.navigateForward('listagem-patrimonios')
+      }
+    })
+  }
+
   ngOnInit() {
     this.patrimonioForm = this.formBuilder.group({
       plaqueta: ['', Validators.required],
@@ -69,29 +116,29 @@ export class CadastrarPatrimonioPage implements OnInit {
   /********************************************************\
                   MENSAGEM DE ALERTA 
   \********************************************************/
-  async alerta() {
-    const alert = await this.alertController.create({
-      header: 'Patrimônio cadastrado com sucesso',
-      message: 'Deseja cadastrar outro patrimonio?',
-      buttons: [
-        {
-          text: 'SIM',
-          role: 'sim',
-          handler:()=>{
-            window.location.reload()
-          }
-        },
-        {
-          text: 'NÃO',
-          role: 'nao',
-          handler:()=>{
-            this.nav.navigateForward('listagem-patrimonios')
-          }
-        }
-      ]
-    });
-
-    await alert.present();
+  alerta() {
+    Swal.fire({
+      heightAuto: false, // Remove o 'heigth' que estava definido nativamente, pois ele quebra o estilo da pagina
+      allowOutsideClick: false, // Ao clicar fora do alerta ele não vai fechar
+      title: 'SUCESSO',
+      text: 'O cadastro do patrimônio foi incluido no sistema. Deseja cadastrar outro patrimônio?',
+      icon: 'success',
+      showCancelButton: true,
+      confirmButtonText: 'SIM',
+      cancelButtonText: 'NÃO',
+      // Customizção
+      confirmButtonColor: 'var(--ion-color-success-tint)',
+      cancelButtonColor: 'var(--ion-color-danger-tint)',
+      backdrop: `linear-gradient(#a24b7599 100%, transparent 555%)`
+    }).then((result) => {
+      if (result.isConfirmed) { // Se o resultado for 'SIM', faça isso
+        window.location.reload()
+      } else if (
+        result.dismiss === Swal.DismissReason.cancel // Se o resultado for 'NÃO', faça isso
+      ) {
+        this.nav.navigateForward('listagem-patrimonios')
+      }
+    })
   }
 
 }
