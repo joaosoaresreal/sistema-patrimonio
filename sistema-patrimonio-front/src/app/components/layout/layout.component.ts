d687e05eb2f6f885e9bf94fb8df1914440525ee1
joaosoaresreal <joao.soares8@estudante.ifms.edu.br>
2024-02-04 15:46:18 -0400

    Componentizando o Layout geral

---
+++ sistema-patrimonio-front/src/app/components/layout/layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss'],
+})
+export class LayoutComponent  implements OnInit {
+  @Input() pagina: string = '';
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
