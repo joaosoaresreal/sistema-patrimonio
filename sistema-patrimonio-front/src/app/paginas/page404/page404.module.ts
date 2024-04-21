054efc336b39c9c02895159846a63c51899bff38
João Soares <joao.soares8@estudante.ifms.edu.br>
2024-04-20 22:12:58 -0400

    modified:   sistema-patrimonio-front/src/app/paginas/page404/page404.module.ts 	modified:   sistema-patrimonio-front/src/app/paginas/page404/page404.page.html 	modified:   sistema-patrimonio-front/src/app/paginas/page404/page404.page.scss

--- sistema-patrimonio-front/src/app/paginas/page404/page404.module.ts
+++ sistema-patrimonio-front/src/app/paginas/page404/page404.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { Page404PageRoutingModule } from './page404-routing.module';
 
 import { Page404Page } from './page404.page';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    Page404PageRoutingModule
+    Page404PageRoutingModule,
+    SharedModule
   ],
   declarations: [Page404Page]
 })
