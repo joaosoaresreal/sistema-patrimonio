b0614cb764321811ff256524abe33773e46820a9
João Soares <joao.soares8@estudante.ifms.edu.br>
2024-07-04 19:44:09 -0400

    Ajuste para exibir nome do usuario na tela home

--- sistema-patrimonio-front/src/app/paginas/home/home.page.ts
+++ sistema-patrimonio-front/src/app/paginas/home/home.page.ts
@@ -2,28 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { AuthenticationService } from 'src/app/services/domain/Authentication.service';
 
-
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-
-
 export class HomePage implements OnInit {
 
   // Gerando a data do dia, de forma automatizada
   today: number = Date.now();
-  
-  constructor(public nav: NavController, protected auth: AuthenticationService) { }
-  
+
+  constructor(
+    public nav: NavController,
+    protected auth: AuthenticationService
+  ) { }
+
+  usuarioHome = this.auth.dadosUsuario().nomeUsuario
+
   abrirPagina(page: string){
     this.nav.navigateForward(page)
   }
 
-
   ngOnInit() {
   }
-
 }
